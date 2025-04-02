0fa9eb06f84059631f45d504d01b32727d05e0f0
Mustafa Fedayi <mustafafedayi@macbookpro.myfiosgateway.com>
2025-04-01 23:19:51 -0400

    Block24

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,39 @@ import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppyList);
+
+  const featuredPop = puppies.find((pup) => pup.id === featPupId);
+
   return (
     <>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {" "}
+            {/* end of ptag */}
+            {puppy.name}
+          </p>
+        );
       })}
+
+      <div>
+        {featPupId && (
+          <div>
+            <h3>{featuredPop.name}</h3>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">{featuredPop.age}</li>
+              <li>{featuredPop.isCute && "Cute"}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   );
 }
